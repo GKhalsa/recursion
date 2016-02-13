f4114c111812b3d6d94112f89e339c57a2294467
Gurusundesh Khalsa <gurusundesh@khalsa.com>
2016-02-12 21:08:51 -0700

    refactor, need to get odd wokring

--- collatz.rb
+++ collatz.rb
@@ -1,13 +1,10 @@
 def collatz(n)
   array = []
-  if n == 1
-    # array << n
-    return array
-  end
-    n = n/2 if n.even?
-    n = 3*n + 1 if n.odd?
-    array = collatz(n)
-    array << n
+  return array if n == 1
+  n = n/2 if n.even?
+  #  n = (3*n + 1) if n.odd?
+  array = collatz(n)
+  array << n
 end
 
 collatz(8)
